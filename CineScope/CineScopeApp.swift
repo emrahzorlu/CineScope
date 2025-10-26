76e20c51dbf4d36667961668968d7973b8e1b2dd
Emrah Zorlu <emrahzorlu_2002@icloud.com>
2025-10-26 14:39:02 +0300

    Initial Commit

---
+++ CineScope/CineScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineScopeApp.swift
+//  CineScope
+//
+//  Created by Emrah Zorlu on 26.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct CineScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
